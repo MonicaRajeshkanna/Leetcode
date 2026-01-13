ae1eff4a7e3bdbe9758c601139525b3cfe5ba711
Monica <monicarajesh1107@gmail.com>
2026-01-13 15:03:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (80.73%) - LeetSync

---
+++ 86-partition-list/partition-list.py
@@ -0,0 +1,26 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def partition(self, head, x):
+        less_dummy = ListNode(0)
+        greater_dummy = ListNode(0)
+
+        less = less_dummy
+        greater = greater_dummy
+
+        while head:
+            if head.val < x:
+                less.next = head
+                less = less.next
+            else:
+                greater.next = head
+                greater = greater.next
+            head = head.next
+
+        greater.next = None
+        less.next = greater_dummy.next
+
+        return less_dummy.next
